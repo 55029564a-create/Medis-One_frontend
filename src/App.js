446cd33ec06a755433eb94824b1506eca6c46511
LTS <55029564a@gmail.com>
2026-01-19 11:24:00 +0900

    리액트수정본1

--- src/App.js
+++ src/App.js
@@ -35,44 +35,42 @@ import ProcessMgmt from "./pages/Admin/ProcessMgmt";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* 로그인 페이지 (별도 레이아웃) */}
-        <Route path="/" element={<Login />} />
+    <Routes>
+      {/* 로그인 페이지 (별도 레이아웃) */}
+      <Route path="/" element={<Login />} />
 
-        {/* 메인 레이아웃이 적용되는 페이지들 */}
-        <Route element={<Layout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
+      {/* 메인 레이아웃이 적용되는 페이지들 */}
+      <Route element={<Layout />}>
+        <Route path="/dashboard" element={<Dashboard />} />
 
-          {/* 자재 */}
+        {/* 자재 */}
 
-          <Route path="/material/inout" element={<MaterialInout />} />
-          <Route path="/material/history" element={<MaterialHistory />} />
-          <Route path="/inventory" element={<InventoryCurrent />} />
+        <Route path="/material/inout" element={<MaterialInout />} />
+        <Route path="/material/history" element={<MaterialHistory />} />
+        <Route path="/inventory" element={<InventoryCurrent />} />
 
-          {/* 생산 */}
-          <Route path="/production/order" element={<WorkOrder />} />
-          <Route path="/production/schedule" element={<ProductionSchedule />} />
-          <Route path="/production/report" element={<WorkReport />} />
+        {/* 생산 */}
+        <Route path="/production/order" element={<WorkOrder />} />
+        <Route path="/production/schedule" element={<ProductionSchedule />} />
+        <Route path="/production/report" element={<WorkReport />} />
 
-          {/* 설비 & 품질 */}
-          <Route path="/equipment" element={<MachineList />} />
-          <Route path="/quality/defect" element={<QualityDefect />} />
-          <Route path="/quality/rate" element={<ProductionRate />} />
+        {/* 설비 & 품질 */}
+        <Route path="/equipment" element={<MachineList />} />
+        <Route path="/quality/defect" element={<QualityDefect />} />
+        <Route path="/quality/rate" element={<ProductionRate />} />
 
-          {/* 지원 업무 */}
-          <Route path="/support/notice" element={<Notice />} />
-          <Route path="/support/cafeteria" element={<CafeteriaMenu />} />
+        {/* 지원 업무 */}
+        <Route path="/support/notice" element={<Notice />} />
+        <Route path="/support/cafeteria" element={<CafeteriaMenu />} />
 
-          {/* 관리자 */}
-          <Route path="/admin/employees" element={<EmployeeMgmt />} />
-          <Route path="/admin/process" element={<ProcessMgmt />} />
-        </Route>
+        {/* 관리자 */}
+        <Route path="/admin/employees" element={<EmployeeMgmt />} />
+        <Route path="/admin/process" element={<ProcessMgmt />} />
+      </Route>
 
-        {/* 없는 페이지 접속 시 로그인으로 보냄 */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
+      {/* 없는 페이지 접속 시 로그인으로 보냄 */}
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 }
 
